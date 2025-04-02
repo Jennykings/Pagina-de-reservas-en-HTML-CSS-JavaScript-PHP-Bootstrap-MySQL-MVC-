f1f46a67a07ad4182681ee35da15215967431cca
Jennykings <jennykings51@gmail.com>
2025-04-02 01:11:41 -0500

    update login y img

--- views/inicio_sesion/index.php
+++ views/inicio_sesion/index.php
@@ -8,11 +8,11 @@
         <title>Login</title>
 
 <!--favicon-->
-<link rel="shortcut icon" href="/RESTAURANTE/views/img/utencilios.png" type="image/x-icon">
+<link rel="shortcut icon" href="../img/utencilios.png" type="image/x-icon">
  
     
         <!-- css-->
-        <link rel="stylesheet" href="/RESTAURANTE/views/inicio_sesion/css/login.css">
+        <link rel="stylesheet" href="./css/login.css">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -26,13 +26,13 @@
             <div class="Caja">
 
              
-            <form  action="/RESTAURANTE/controllers/controlador_login.php" method="post"> 
+            <form  action="../../controllers/controlador_login.php" method="post"> 
    <h1 class="animate__animated animate__backInLeft">Inicie sesión</h1>
-   <p class="add">¿Todavia no tienes una cuenta? <a href="/RESTAURANTE/views/inicio_sesion/index_register.html" class="reg" >Registrarse</a></p>
+   <p class="add">¿Todavia no tienes una cuenta? <a href="./index_register.html" class="reg" >Registrarse</a></p>
    <p class="blanco">Usuario <input class="input100" type="text" id="usuario" name="usuario" placeholder="Usuario"></p>
    <p class="blanco">Contraseña <input class="input100" type="password" id="password" name="password" placeholder="Password"></p>
    <input class="btn btn-warning" type="submit" value="Ingresar"> <br> <br>
-    <a href="/RESTAURANTE/index.html" class="reg" > - Volver a la pagina principal -</a>
+    <a href="../../index.html" class="reg" > - Volver a la pagina principal -</a>
    </form> 
     </div> 
      
